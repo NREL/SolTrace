84f63c53f2afb285c2e9938f2f506b8ec0a3d0e3
mjwagner2 <mjwagner2@gmail.com>
2023-01-13 15:54:44 -0600

    updating python api test.py file

--- app/deploy/api/test.py
+++ app/deploy/api/test.py
@@ -18,19 +18,19 @@ opt_abs.reflectivity = 0.
 # Sun
 sun = PT.add_sun()
 # Give sun an arbitrary position
-sun.position.x = -20.
-sun.position.y = 0
+sun.position.x = 100.
+sun.position.y = 0.
 sun.position.z = 100.
 
 # Reflector stage
 st = PT.add_stage()
 
 # Simple reflector at origin
-# st.add_elements()
-# el = st.elements[-1]
+# el = st.add_element()
 # el.aim.z = 1
 # el.optic = opt_ref
-# el.Create()
+# el.surface_flat()
+# el.aperture_rectangle(1,1)
 
 #absorber element height
 abs_pos = Point(0., 0., 10.)
@@ -39,6 +39,7 @@ abs_pos = Point(0., 0., 10.)
 for i in range(5):
     hpos = [random.uniform(-10,10), random.uniform(-10,10)]
     el = st.add_element()
+    el.optic = opt_ref
     el.position.x = hpos[0]
     el.position.y = hpos[1]
     # calculate the vectors - receiver, sun, and aim
@@ -48,9 +49,11 @@ for i in range(5):
     # assign the aim vector. scale by a large number
     el.aim = el.position + avec*100.
     # compute surface z rotation to align with plane of the ground
-    # el.zrot = PT.util_calc_zrot_azel(avec)
+    el.zrot = PT.util_calc_zrot_azel(avec)
+    # Set surface and aperture characteristics
+    el.surface_flat()
+    el.aperture_rectangle(0.5,1.0)
     
-    el.optic = opt_ref
 
 # absorber stage
 sta = PT.add_stage()
@@ -59,6 +62,7 @@ ela = sta.add_element()
 ela.position = abs_pos
 ela.aim.z = 0.
 ela.optic = opt_abs
+ela.surface_flat()
 ela.aperture_rectangle(5,5)  #target is 5x5 
 
 # set simulation parameters
@@ -71,9 +75,7 @@ PT.is_surface_errors = True
 
 if __name__ == "__main__":
 
-    # h = thread_helper()
-    # h.thread_id = 4
-    PT.run(-1, True, 4)
+    PT.run(-1, True, 4)         #(seed, is point focus system?, number of threads)
     
     print("Num rays traced: {:d}".format(PT.raydata.index.size))
 
@@ -84,10 +86,9 @@ if __name__ == "__main__":
     loc_z = df.loc_z.values
 
 
+    # Plotting with plotly
     import plotly.express as px 
     import plotly.graph_objects as go
-
-    # fig = px.scatter_3d(df, x='loc_x', y='loc_y', z='loc_z', color='stage', size='element') 
 
     fig = go.Figure(data=go.Scatter3d(x=loc_x, y=loc_y, z=loc_z, mode='markers', marker=dict( size=1, color=df.stage, colorscale='bluered', opacity=0.8, ) ) )
 
