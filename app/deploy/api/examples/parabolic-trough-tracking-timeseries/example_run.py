d9b25360d43fee375da7de493f9517b5fbf24f63
Brooke Stanislawski <34248707+brookeslawski@users.noreply.github.com>
2024-01-12 23:22:19 -0700

    pysoltrace for time series data of tilt angle measurements in parabolic trough systems (#48)
    * commenting out app
    
    * initial working version on mac
    
    * working parabolic trough example
    
    * adding flux distribution computation
    
    * created post-proc script and added optical efficiency calc
    
    * improved flux map calc of rolled-up sfc
    
    * test problem
    
    * added unrolled receiver cylinder surface flux map
    
    * validating LS3 case
    
    * looping over sun positions, trough stationary
    
    * rotating trough based on sun position with angle input
    
    * running soltrace with field tilt data
    
    * diurnal cycle sweep with improved plotting
    
    * moved angles plotting to a function
    
    * switched around loops to keep name main loop as outer
    
    * restructured code to loop over rows of field data
    
    * loops over rows with improved plotting
    
    * adding env yaml file
    
    * small tweaks for nominal and field inputs
    
    * validating deviation with intercept factor yang 2022
    
    * testing for field mode
    
    * fixed intercept factor calc for single stage
    
    * minor fixes
    
    * fixed sun pos and trough angle calcs
    
    * adding running soltrace for stats and median diurnal cycle
    
    * updating plotting script for diurnal cycle
    
    * converting pickle file to csv for A Zolan
    
    * cleaning up code and adding functions
    
    * added functions for field-data-post-processing.py
    
    * dividing up postproc functs
    
    * working version with char run type
    
    * Create README-pysoltrace-trackingerror
    
    Instructions for using PySolTrace for Tracking Error Measurements (main-runsoltrace-iterate.py)
    
    * Rename README-pysoltrace-trackingerror to README-pysoltrace-trackingerror.md
    
    * Rename README-pysoltrace-trackingerror.md to README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * more clean up
    
    * converting pysoltrace iterate script to a function
    
    * creating wrapper function
    
    * improving wrapper function
    
    * adding first regression test
    
    * fixed intercept factor calc for single stage and added tests
    
    * adding nominal mode
    
    * zeroing out y sun pos to eliminate edge losses
    
    * simplifying and cleaning up figures for paper
    
    * adding median char day capability to run_soltrace
    
    * adding stats mode
    
    * merging changes on loaner laptop with git repo
    
    * testing branch editing in spyder
    
    * preparing demo for NREL/SolTrace PR
    
    * removed load_field_data from run_pysoltrace_iterate for demo
    
    * clean up of main function and correcting path names in test files
    
    * edits to pass all tests
    
    * removing files not needed for demo
    
    * removing files not needed for demo
    
    * removing files not needed for demo
    
    * removing files not needed for demo
    
    * updating README
    
    * removing personal paths where possible
    
    * adding reference to description
    
    * commit before switching to original laptop
    
    * move example contents into examples directory
    
    * renaming and adding field data
    
    * changing paths to run in new directory
    
    * merged NREL/SolTrace:develop into local branch, updated paths in all test files too, updated README
    
    * final clean up for pull request
    
    * removing pickle dependency, using csv instead
    
    * updating readme to remove .p and add .csv

---
+++ app/deploy/api/examples/parabolic-trough-tracking-timeseries/example_run.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 25 11:11:23 2023
+
+@author: bstanisl
+
+This is an illustrative example using a snippet of field measurement data (`demo_field_data.py`) from the Nevada Solar One CSP plant [1].
+
+[1] National Renewable Energy Laboratory (NREL). (2021). Wind and Structural Loads on Parabolic Trough Solar Collectors at Nevada Solar One [data set].  Retrieved from https://dx.doi.org/10.25984/2001061.
+
+"""
+import pandas as pd
+
+from st_processing_functions import *
+from run_pysoltrace_iterate import *
+
+
+#% INPUTS ===========================================================================================
+
+# define constant inputs                                                                                                                                                                                                                                                                                                                       
+sunshape_flag = False
+sfcerr_flag = False
+optics_type = 'realistic' # 'ideal'
+plot_rays = False # plot rays in ray-tracing simulation using plotly - this can be slow with more than 1e4 rays
+save_pickle = False # save results as pickle file (must install pickle in env and uncomment lines in run_soltrace_iterate.py)
+n_hits = 1e5 #1e5 # 5e6 # 1e5 
+nx = 30
+ny = 30
+
+# parabolic trough geometry definition ================================
+# Nevada Solar One Trough Geometry (LS-2)
+module_length = 12.0 # module length
+aperture_width = 5.0 #5.77 # aperture width
+focal_length = 1.49 #1.71 # focal length # this must be correct for results to make sense
+d_abstube = 0.07 # diameter of absorber tube
+ptc_pos = [0, 0, 0] # x, y, z
+ptc_aim = [0, 0, 1] # x, y, z
+
+# Nevada Solar One field site information
+lat, lon = 35.8, -114.983 #coordinates of NSO
+altitude = 543 #m
+
+# running with field data timeseries =============================================
+tracker_angle_input_mode = 'field' # 'validation' 'nominal'
+sensorlocs = ['R4_DO','R4_SO'] # sensor location names
+field_data = pd.read_csv('example_field_data.csv', index_col=0, parse_dates=True)
+
+if __name__ == "__main__":
+    results, df = run_soltrace_iterate(field_data, lat, lon, altitude, tracker_angle_input_mode, sensorlocs, 
+                                       module_length, aperture_width, focal_length, d_abstube, 
+                                       ptc_pos, ptc_aim, sunshape_flag, sfcerr_flag, 
+                                       optics_type, plot_rays, save_pickle, n_hits, nx, ny)
