1b490242bb47f7f03f945356bfe43d850beff5bb
Taylor Brown <60201147+taylorbrown75@users.noreply.github.com>
2025-11-14 13:47:32 -0700

    Initialize elevation in SunPositionCalculator

--- coretrace/simulation_data/solar_position_calculators/solar_position_calculator.cpp
+++ coretrace/simulation_data/solar_position_calculators/solar_position_calculator.cpp
@@ -19,7 +19,7 @@ SolarPositionCalculator::SolarPositionCalculator()
         dut1(0.0), altitude(0.0), pressure(1013.25), temperature(20.0),
         location_set(false), date_set(false), time_set(false), calculated(false),
         Azimuth(0.0), Zenith(0.0),
-        X(0.0), Y(0.0), Z(0.0)
+        X(0.0), Y(0.0), Z(0.0), Elevation(0.0)
 {
 }
 
